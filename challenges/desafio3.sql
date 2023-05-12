c55761552977087cb8136f22a686c8be80f0256b
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-05-12 14:16:13 -0300

    Requisito 3 ok

---
+++ challenges/desafio3.sql
@@ -0,0 +1,10 @@
+SELECT 
+	p.nome AS pessoa_usuaria,
+	COUNT(h.musica_id) AS musicas_ouvidas,
+    ROUND(SUM(m.duracao)/60, 2) AS total_minutos
+FROM pessoas_usuarias AS p
+    
+INNER JOIN historico_reproducoes AS h ON p.id = h.usuario_id
+INNER JOIN musicas AS m on h.musica_id = m.id
+
+GROUP BY p.nome ORDER BY p.nome;
