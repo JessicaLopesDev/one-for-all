2d2daa0b71d12492d61e6e11638916414a44a7d8
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-05-15 15:47:07 -0300

    Requisito 4 ok

---
+++ challenges/desafio4.sql
@@ -0,0 +1,7 @@
+SELECT p.nome AS pessoa_usuaria,
+  IF (MAX(YEAR(h.data_reproducao)) >= 2021, 'Ativa', 'Inativa') status_pessoa_usuaria
+FROM pessoas_usuarias AS p
+
+INNER JOIN historico_reproducoes AS h ON p.id = h.usuario_id
+
+GROUP BY pessoa_usuaria ORDER BY pessoa_usuaria;
